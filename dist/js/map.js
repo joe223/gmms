c3b48622c8759a19d45e2abedc033735af4a932c
joe223 <heyin223@163.com>
2017-03-14 22:11:16 +0800

    joe223 change: delete minimap

--- dist/js/map.js
+++ dist/js/map.js
@@ -2,10 +2,27 @@
  * Created by Administrator on 2017/3/13 0013.
  */
 function initMap() {
-    var map = L.map('map', {zoomControl: false}).setView([32.324, 103.434], 5);
+    var map = L.map('map', {
+        zoomControl: false
+    }).setView([32.324, 103.434], 5);
+
+    // 全屏地图控件
+    L.control.fullscreen({
+        position: 'topright',
+    }).addTo(map);
+
+    // 缩放地图控件
     L.control.zoom({
         position: 'topright'
     }).addTo(map);
+
+    // 右下角 mini 地球控件，暂时不需要
+    // new L.Control.GlobeMiniMap({
+    //     topojsonSrc: 'dist/json/world.json'
+    // }).addTo(map);
+
+    var sidebar = L.control.sidebar('sidebar').addTo(map);
+
     tileMap = L.tileLayer("http://t0.tianditu.cn/vec_w/wmts?" +
         "SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles" +
         "&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}");
@@ -13,28 +30,16 @@ function initMap() {
     labelMap = L.tileLayer("http://t0.tianditu.com/cva_w/wmts?" +
         "SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles" +
         "&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}", {
-        attribution: '&copy; <a href="http://www.tianditu.com/">天地图</a> contributors'
-    });
+            attribution: '&copy; <a href="http://www.tianditu.com/">天地图</a> contributors'
+        });
 
     var cities = L.layerGroup([tileMap, labelMap]);
+
     map.addLayer(cities);
 
     // L.tileLayer('http://120.55.74.101:8012/mapbox-studio-outdoors/{z}/{x}/{y}.png', {
     //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     // }).addTo(map);
-    /*
-    * add map control
-    * */
-    L.control.fullscreen({
-        position: 'topright',
-    }).addTo(map);
-    new L.Control.GlobeMiniMap({
-        topojsonSrc: 'dist/json/world.json'
-    }).addTo(map);
-
-    var sidebar = L.control.sidebar('sidebar').addTo(map);
-
 
     return map;
 };
-
